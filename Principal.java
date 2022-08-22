de1e9b5747e6f63b91953d678661f6ca6c1cb08a
LeonardoCarrillo <carrillomartinezleonardojavier@gmail.com>
2022-08-22 18:09:27 -0500

    Este es mi propio git

---
+++ Principal.java
@@ -0,0 +1,13 @@
+public class Principal{
+
+  int numero1 = 21;
+  int nombre= "leonardo";
+
+  public static void main(String[] args) {
+
+    System.out.println("hola");
+    System.out.println(nombre + " " + tienes + numero1 + "anios");
+
+  }
+
+}
